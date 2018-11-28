fa2fc9274f9c69daf56dad643cca61dd67a2a903
JonteNeander <jonte.neander@gmail.com>
2018-11-28 14:09:26 +0100

    visitor

--- JonTimExamen/JonTimExamen/Data/WebDbContext.cs
+++ JonTimExamen/JonTimExamen/Data/WebDbContext.cs
@@ -7,6 +7,7 @@ namespace JonTimExamen.Data
     public class WebDbContext : IdentityDbContext<Employee>
     {
         public DbSet<Employee> Employee { get; set; }
+        public DbSet<Visitor> Visitor { get; set; }
 
         public WebDbContext(DbContextOptions<WebDbContext> options) : base(options)
         {
