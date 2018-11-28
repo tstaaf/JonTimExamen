76cc2ca539c81a67083cf72ef9debe67b5ed6796
tstaaf <timmystaaf@gmail.com>
2018-11-28 13:24:35 +0100

    test

--- JonTimExamen/JonTimExamen/Data/WebDbContext.cs
+++ JonTimExamen/JonTimExamen/Data/WebDbContext.cs
@@ -7,6 +7,7 @@ namespace JonTimExamen.Data
     public class WebDbContext : IdentityDbContext<Employee>
     {
         public DbSet<Employee> Employee { get; set; }
+        public DbSet<Appointment> Appointment { get; set; }
 
         public WebDbContext(DbContextOptions<WebDbContext> options) : base(options)
         {
