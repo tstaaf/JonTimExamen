394d89429115e8a221a16c81ad718a23bdecb980
jonteneander <33481610+jonteneander@users.noreply.github.com>
2019-02-25 20:23:22 +0100

    fix

--- JonTimExamen/JonTimExamen/wwwroot/js/site.js
+++ JonTimExamen/JonTimExamen/wwwroot/js/site.js
@@ -1,24 +1,19 @@
-﻿<script language="JavaScript">
- Webcam.set({
-      width: 320,
-height: 240,
-image_format: 'jpeg',
-jpeg_quality: 90
-});
-Webcam.attach('#my_camera');
-</script>
+﻿    Webcam.set({
+        width: 320,
+        height: 240,
+        image_format: 'jpeg',
+        jpeg_quality: 90
+    });
+Webcam.attach('camera');
 
-    <script language="JavaScript">
-        function take_snapshot() {
-            Webcam.snap(function (data_uri) {
-                document.getElementById('results').innerHTML =
-                '<img src="' + data_uri + '"/>';
 
-            Webcam.upload(data_uri,
-                '/Camera/Capture',
-                function (code, text) {
-                    alert('Photo Captured');
-                });
-                })
-            }
-    </script> 
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById('results').innerHTML = '<img src="' + data_uri + '"/>';
+
+        Webcam.upload(data_uri, function (code, text) {
+            alert('Photo Captured');
+        });
+    });
+}
