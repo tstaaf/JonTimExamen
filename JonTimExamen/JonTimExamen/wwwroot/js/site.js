c2791eacb91d7102bf8a508d13e1e09ca0794314
tstaaf <timmystaaf@gmail.com>
2019-03-28 20:11:10 +0100

    finafisken

--- JonTimExamen/JonTimExamen/wwwroot/js/site.js
+++ JonTimExamen/JonTimExamen/wwwroot/js/site.js
@@ -1,18 +1,18 @@
-﻿    Webcam.set({
-        width: 320,
-        height: 240,
-        image_format: 'jpeg',
-        jpeg_quality: 90
-    });
-Webcam.attach('#camera');
+﻿//    Webcam.set({
+//        width: 320,
+//        height: 240,
+//        image_format: 'jpeg',
+//        jpeg_quality: 90
+//    });
+//Webcam.attach('#camera');
 
 
 
-function takePicture() {
-    Webcam.snap(function (data_uri) {
-        document.getElementById('results').innerHTML = '<img src="' + data_uri + '"/>';
+//function takePicture() {
+//    Webcam.snap(function (data_uri) {
+//        document.getElementById('results').innerHTML = '<img class="center-block" src="' + data_uri + '"/>';
 
-        Webcam.upload(data_uri, '/Visitor/Capture');
-    });
+//        Webcam.upload(data_uri, '/Visitor/Capture');
+//    });
 
-}
+//}
