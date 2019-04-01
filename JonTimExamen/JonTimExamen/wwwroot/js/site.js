96bc87f45af40653c2fc7cd254d53d050f71a644
tstaaf <timmystaaf@gmail.com>
2019-04-01 21:47:04 +0200

    df

--- JonTimExamen/JonTimExamen/wwwroot/js/site.js
+++ JonTimExamen/JonTimExamen/wwwroot/js/site.js
@@ -51,6 +51,8 @@ function checkoutButtonClicked() {
     }, 4500)
 }
 
-//function showInfoText() {
-//    $("#infoText")
-//}
+function showInfoText() {
+    $("#infoLink").on('click', function () {
+        $("#Info").toggleClass("hidden");
+    });
+}
