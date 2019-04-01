54208e49e02213d1448d7c7eb2387a041b620623
JonteNeander <jonte.neander@gmail.com>
2019-04-01 20:26:43 +0100

    fix

--- JonTimExamen/JonTimExamen/wwwroot/js/site.js
+++ JonTimExamen/JonTimExamen/wwwroot/js/site.js
@@ -1,18 +1,56 @@
-﻿//    Webcam.set({
-//        width: 320,
-//        height: 240,
-//        image_format: 'jpeg',
-//        jpeg_quality: 90
-//    });
-//Webcam.attach('#camera');
+﻿Webcam.set({
+    width: 320,
+    height: 240,
+    image_format: 'jpeg',
+    jpeg_quality: 90
+});
+Webcam.attach('#camera');
 
+function takePicture() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById('results').innerHTML =
+            '<img class="center-block" src="' +
+            data_uri +
+            '"/>';
 
+        Webcam.upload(data_uri,
+            '/Visitor/Capture'
+    )});
+}
 
-//function takePicture() {
-//    Webcam.snap(function (data_uri) {
-//        document.getElementById('results').innerHTML = '<img class="center-block" src="' + data_uri + '"/>';
+function removeAndShowButtons() {
+    $("#finishButton").show();
+    $("#pictureButton").hide();
+    $("#camera").hide();
+}
 
-//        Webcam.upload(data_uri, '/Visitor/Capture');
-//    });
+function hideAndShow() {
+    $("#qr").show().css({ "margin": "auto" , "background-color": "white" , "color": "black" , "text-align": "center" , "border-radius": "25px"});
+    $("#backBtn").show().css({ "position": "absolute" , "right": "15px" , "top": "520px"});
+    $("#pic").hide();
 
+}
+
+function textAndBack() {
+    $("#qr").hide();
+    $("#backBtn").hide();
+    $("#captureHeroText").show();
+    $("#captureHeroH1").fadeIn(1500);
+    $("#captureHeroH2").delay(1800).fadeIn(1500);
+    setTimeout(function () {
+        window.location = "/Home/Index";
+    }, 4500)
+}
+
+function checkoutButtonClicked() {
+    $("#formDiv").hide();
+    $("#checkoutHeroText").show();
+    $("#checkoutHeroH1").fadeIn(1500);
+    setTimeout(function () {
+        $("#checkoutForm").submit();
+    }, 4500)
+}
+
+//function showInfoText() {
+//    $("#infoText")
 //}
